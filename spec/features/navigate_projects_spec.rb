b43547d5092d6f5530c0e351991799a02e211883
Louisa Barrett <louisabarrett@gmail.com>
2014-01-12 16:06:11 -0700

    refactored views

--- spec/features/navigate_projects_spec.rb
+++ spec/features/navigate_projects_spec.rb
@@ -3,11 +3,7 @@ require 'spec_helper'
 describe 'Navigating projects' do
 
   it 'allows navigation from the detail page to the listing page' do
-    project = Project.create( name: "Sporks-a-Million",
-                              description: "Saving the world one spork at a time.",
-                              target_pledge_amount: 700.00,
-                              pledging_ends_on:  1.day.from_now,
-                              website: "http://sporksamillion.com" )
+    project = Project.create( project_attributes )
 
     visit project_url(project)
 
@@ -17,11 +13,7 @@ describe 'Navigating projects' do
   end
 
   it 'allows navigation from the listing page to the detail page' do
-    project = Project.create( name: "Sporks-a-Million",
-                              description: "Saving the world one spork at a time.",
-                              target_pledge_amount: 700.00,
-                              pledging_ends_on:  1.day.from_now,
-                              website: "http://sporksamillion.com" )
+    project = Project.create( project_attributes )
 
     visit projects_url
 
--- spec/features/show_project_spec.rb
+++ spec/features/show_project_spec.rb
@@ -3,11 +3,8 @@ require 'spec_helper'
 describe "Viewing an individual project" do
 
   it "shows the project's details" do
-    project = Project.create( name: "Sporks-a-Million",
-                              description: "Saving the world one spork at a time.",
-                              target_pledge_amount: 700.00,
-                              pledging_ends_on:  1.day.from_now,
-                              website: "http://sporksamillion.com" )
+    project = Project.create( project_attributes )
+    
     visit project_url(project)
 
     expect(page).to have_text(project.name)
