92e2d1bd59e7ff41c435f0405eeeed48254a416d
Louisa Barrett <louisabarrett@gmail.com>
2014-01-15 16:19:00 -0700

    fixed list_projects_spec.rb

--- spec/features/show_project_spec.rb
+++ spec/features/show_project_spec.rb
@@ -10,8 +10,24 @@ describe "Viewing an individual project" do
     expect(page).to have_text(project.name)
     expect(page).to have_text(project.description)
     expect(page).to have_text("$700.00")
-    expect(page).to have_text(project.pledging_ends_on)
+    expect(page).to have_text("1 day remaining")
     expect(page).to have_text(project.website)
+  end
+
+  it "shows the days remaining if the pledging end date is in the future" do
+    project = Project.create(project_attributes(pledging_ends_on: 1.day.from_now))
+
+    visit project_url(project)
+
+    expect(page).to have_text("1 day remaining")
+  end
+
+  it "shows 'All Done!' if the pledging end date is in the past" do
+    project = Project.create(project_attributes(pledging_ends_on: 2.days.ago))
+
+    visit project_url(project)
+
+    expect(page).to have_text("All Done!")
   end
 
 end
