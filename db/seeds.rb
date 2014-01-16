2c70b3104aa058864760c965fbdf7b4dbf1c3215
Louisa Barrett <louisabarrett@gmail.com>
2014-01-16 08:04:32 -0700

    added project info to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Project.create!([
+  { name: "Sporks-a-Million",
+    description: "Saving the world one spork at a time.",
+    target_pledge_amount: 700.00,
+    pledging_ends_on:  1.day.from_now,
+    website: "http://sporksamillion.com"
+  },
+  { name: "Kids Count",
+    description: "Helping kids in underserved areas get excited about math.",
+    target_pledge_amount: 1000.00,
+    pledging_ends_on: 1.week.from_now,
+    website: "http://kids_count.com"
+  },
+  { name: "Send Me On Vacation",
+    description: "My name is Bob and I'm raising other people's money for a personal vacation!",
+    target_pledge_amount: 500.00,
+    pledging_ends_on: 1.month.from_now,
+    website: "http://send_bob_to_hawaii.net"
+  }
+])
